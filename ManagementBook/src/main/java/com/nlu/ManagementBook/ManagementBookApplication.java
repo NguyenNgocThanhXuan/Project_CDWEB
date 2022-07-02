f58f767957aac65e215455825c6965b155d04334
unknown <18130293@st.hcmuaf.edu.vn>
2022-07-02 21:27:04 +0700

    Update Project

---
+++ ManagementBook/src/main/java/com/nlu/ManagementBook/ManagementBookApplication.java
@@ -0,0 +1,13 @@
+package com.nlu.ManagementBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementBookApplication.class, args);
+	}
+
+}
